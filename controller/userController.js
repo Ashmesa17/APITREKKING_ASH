d38e5c7950579922de4711372ba3fc2b1d0a25ee
Ashmesa17 <163008316+Ashmesa17@users.noreply.github.com>
2024-11-28 10:43:57 -0500

    Update put user

--- controller/userController.js
+++ controller/userController.js
@@ -23,7 +23,7 @@ const postUser= async (req,res)=>{
     const {Name, Email} = req.body 
     let msg= ' user update' 
     try{ 
-        await User.findOneAndUpdate({Name:Name}, {Name:Name, Email:Email}) 
+        await User.findOneAndUpdate({Name:Name}, {Email:Email}) 
     }catch(error){ msg=error } 
      res.json({msg:msg}) 
 }
