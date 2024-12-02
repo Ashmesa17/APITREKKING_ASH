602931d532036dc24f289e8b8d9ddf4cbdda7f50
Ashmesa17 <jaramillomesaashlley@gmail.com>
2024-12-02 17:51:20 -0500

    Modificacion del put

--- controller/userController.js
+++ controller/userController.js
@@ -1,51 +1,56 @@
-const User= require('../model/user.js');
+const User = require('../model/user.js');
 
-const getUser = async (req, res)=> {
-    const user = await User.find()
+// Obtener todos los usuarios
+const getUser = async (req, res) => {
+    const user = await User.find();
+    res.json({ user });
+};
 
-    res.json({user})
-}
-
-
-const postUser= async (req,res)=>{
-    let msg='User inserted'
-    const body= req.body
-    try{
-        const user = new User (body)
-        await user.save() 
-    }catch(error){
-        msg=error
+// Crear un nuevo usuario
+const postUser = async (req, res) => {
+    let msg = 'User inserted';
+    const body = req.body;
+    try {
+        const user = new User(body);
+        await user.save();
+    } catch (error) {
+        msg = error.message;
     }
-    res.json({msg:msg})
-}
+    res.json({ msg });
+};
 
-const putUser=async (req,res)=>{
-    const {id, Name, Email} = req.body
-    let msg= ' user update'
-    try{
-        await User.findOneAndUpdate({id:id}, {Email:Email, Name:Name})
-    }catch(error){
-        msg=error
+// Actualizar un usuario
+const putUser = async (req, res) => {
+    const { id } = req.params; // ID desde la URL
+    const { Name, Email } = req.body; // Campos a actualizar
+    let msg = 'User updated';
+    try {
+        await User.findByIdAndUpdate(
+            id,
+            { Name, Email }, // Campos a actualizar
+            { new: true, runValidators: true } // Devuelve el documento actualizado y valida
+        );
+    } catch (error) {
+        msg = error.message;
     }
-    res.json({msg:msg})
-}
+    res.json({ msg });
+};
 
-const deleteUser = async (req, res) =>{
-    id= req.params.id
-    let msg= 'User deleted'
-    try{
-        await  User.findOneAndDelete({_id:id})
-
-    }catch (error) {
-        msg= 'There was a problem while deleting '
+// Eliminar un usuario
+const deleteUser = async (req, res) => {
+    const { id } = req.params;
+    let msg = 'User deleted';
+    try {
+        await User.findByIdAndDelete(id);
+    } catch (error) {
+        msg = 'There was a problem while deleting';
     }
-    res.json({msg:msg})
-} 
+    res.json({ msg });
+};
 
-module.exports= {
+module.exports = {
     getUser,
     postUser,
     putUser,
-    deleteUser
-}
-
+    deleteUser,
+};
